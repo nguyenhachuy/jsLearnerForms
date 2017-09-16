38891cf192bdaeab41ec775a5369984539da8ae4
nguyenhachuy <nguyenhachuy@gmail.com>
2017-09-16 11:17:03 -0700

    Finishing first form

--- src/1_first-form.js
+++ src/1_first-form.js
@@ -1,5 +1,13 @@
 (function () {
     'use strict';
 
-    module.exports = {};
+    function buildVector(array) {
+        var vector = array.slice(0);
+        vector.valueOf = function () { return array.slice(0); };
+        vector.toString = function () { return '<' + array.join(',') + '>'; };
+        return vector;
+    }
+    module.exports = {
+        buildVector: buildVector,
+    };
 })();
